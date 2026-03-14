1b7067947ed525e784c6ce17e97a5d1fddbc3eb4
Meenal Kesarkar <159876692+MeenalKesarkar@users.noreply.github.com>
2026-03-14 16:50:51 +0530

    fix CRUD with localStorage

--- src/PostPage.jsx
+++ src/PostPage.jsx
@@ -1,35 +1,51 @@
-import React from 'react'
-import { useParams, Link } from 'react-router-dom'
+import React from "react";
+import { useParams, Link } from "react-router-dom";
 
-const PostPage = ({ posts, handleDelete}) => {
+const PostPage = ({ posts, handleDelete }) => {
+
   const { id } = useParams();
-  const post = posts.find(post => (post.id).toString() === id);
+
+  const post = posts.find((post) => post.id.toString() === id);
 
   return (
-    <main className='PostPage'>
-      <article className='post'>
-        {post &&
-        <>
-          <h2>{post.title}</h2>
-          <p className='postDate'>{post.datetime}</p>
-          <p className='postBody'>{post.body}</p>
-          <Link to={`/edit/${post.id}`}><button className='editButton'>Edit Post</button></Link>
-          <button  className="deleteButton" onClick={() => handleDelete(post.id)}>Delete Post</button>
-        </>
-        }
-        {!post && 
+    <main className="PostPage">
+      <article className="post">
+
+        {post ? (
+          <>
+            <h2>{post.title}</h2>
+
+            <p className="postDate">{post.datetime}</p>
+
+            <p className="postBody">{post.body}</p>
+
+            <Link to={`/edit/${post.id}`}>
+              <button className="editButton">
+                Edit Post
+              </button>
+            </Link>
+
+            <button
+              className="deleteButton"
+              onClick={() => handleDelete(post.id)}
+            >
+              Delete Post
+            </button>
+          </>
+        ) : (
           <>
             <h2>Post Not Found</h2>
             <p>Well, that's disappointing.</p>
+
             <p>
-              <Link to='/'>Visit Our Homepage</Link>
-            </p> 
+              <Link to="/">Visit Our Homepage</Link>
+            </p>
           </>
-        }
-         
+        )}
+
       </article>
     </main>
-  )
-}
+  );
+};
 
-export default PostPage
+export default PostPage;
