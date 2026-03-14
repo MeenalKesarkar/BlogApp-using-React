ab5d02a89a27bbba181b4382615f4e05f1dfd139
Meenal Kesarkar <159876692+MeenalKesarkar@users.noreply.github.com>
2026-03-14 16:25:48 +0530

    made changes

--- src/api/posts.jsx
+++ src/api/posts.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'https://my-json-server.typicode.com/MeenalKesarkar/BlogApp-using-React'
+    baseURL: 'https://my-json-server.typicode.com/MeenalKesarkar/BlogApp-using-React/posts'
 });
